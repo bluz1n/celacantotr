6fc54f4c192ca43815e089e16cabc2587b9064f4
bluz1n <matheusf.ccosta@hotmail.com>
2022-10-01 05:00:08 -0300

    Deploy commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
-import environ
-environ.Env.read_env()
 
 from pathlib import Path
 from env import KEY
@@ -25,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['secret_key']
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
